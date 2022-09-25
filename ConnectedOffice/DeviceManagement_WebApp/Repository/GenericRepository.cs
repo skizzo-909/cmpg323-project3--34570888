6e43904fb92c5670593d06286c257190a94e4b1b
skizzo-909 <nmlambo07@gmail.com>
2022-09-25 17:05:44 +0200

    Added the Repository folder with Generic Class and Interface

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+using DeviceManagement_WebApp.Data;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ConnectedOfficeContext _context;
+        public GenericRepository(ConnectedOfficeContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+            _context.SaveChanges();
+        }
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public T GetById(Guid? id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+            _context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+
+        public bool exists(Guid id)
+        {
+            return _context.Service.Any(e => e.ServiceId == id);
+        }
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(Guid? id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+        bool exists(Guid id);
+    }
+}
