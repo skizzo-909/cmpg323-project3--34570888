7e1e9e44968b43576a0f68a89db555e903c43393
skizzo-909 <nmlambo07@gmail.com>
2022-09-28 00:16:38 +0200

    Update Zone Controller, Repository Class, and Interface

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
@@ -7,25 +7,28 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Repository;
 
 namespace DeviceManagement_WebApp.Controllers
 {
     public class ZonesController : Controller
     {
         private readonly ConnectedOfficeContext _context;
+        private readonly IZonesRepository _zonesRepository;
 
-        public ZonesController(ConnectedOfficeContext context)
+        public ZonesController(ConnectedOfficeContext context, IZonesRepository zonesRepository)
         {
             _context = context;
+            _zonesRepository = zonesRepository;
         }
 
-        // GET: Zones
+        // GET: Index() method that gets all Zones and displays them when the page is loaded
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Zone.ToListAsync());
+            return View(_zonesRepository.GetAll());
         }
 
-        // GET: Zones/Details/5
+        // GET: Details() method that displays the details of a Zone by id
         public async Task<IActionResult> Details(Guid? id)
         {
             if (id == null)
@@ -33,8 +36,7 @@ namespace DeviceManagement_WebApp.Controllers
                 return NotFound();
             }
 
-            var zone = await _context.Zone
-                .FirstOrDefaultAsync(m => m.ZoneId == id);
+            var zone = _zonesRepository.GetById(id);
             if (zone == null)
             {
                 return NotFound();
@@ -49,21 +51,22 @@ namespace DeviceManagement_WebApp.Controllers
             return View();
         }
 
-        // POST: Zones/Create
+        // POST: Create() method to create a Zone by generating a new id then adding it to the database
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ZoneId,ZoneName,ZoneDescription,DateCreated")] Zone zone)
         {
+            // generate a new id for the category
             zone.ZoneId = Guid.NewGuid();
-            _context.Add(zone);
-            await _context.SaveChangesAsync();
+            // add category to database
+            _zonesRepository.Add(zone);
 
             return RedirectToAction(nameof(Index));
         }
 
-        // GET: Zones/Edit/5
+        // GET: Edit() method that gets a Zone by its unique id
         public async Task<IActionResult> Edit(Guid? id)
         {
             if (id == null)
@@ -71,7 +74,7 @@ namespace DeviceManagement_WebApp.Controllers
                 return NotFound();
             }
 
-            var zone = await _context.Zone.FindAsync(id);
+            var zone = _zonesRepository.GetById(id);
             if (zone == null)
             {
                 return NotFound();
@@ -79,7 +82,7 @@ namespace DeviceManagement_WebApp.Controllers
             return View(zone);
         }
 
-        // POST: Zones/Edit/5
+        // POST: Edit() method that gets a Zone by its id then updates any changes
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -93,8 +96,8 @@ namespace DeviceManagement_WebApp.Controllers
 
             try
             {
-                _context.Update(zone);
-                await _context.SaveChangesAsync();
+                // update a Zone and save the changes
+                _zonesRepository.Update(zone);
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -108,10 +111,9 @@ namespace DeviceManagement_WebApp.Controllers
                 }
             }
             return RedirectToAction(nameof(Index));
-
         }
 
-        // GET: Zones/Delete/5
+        // GET: Delete() method that gets a Zone by its id and delete it
         public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
@@ -119,8 +121,7 @@ namespace DeviceManagement_WebApp.Controllers
                 return NotFound();
             }
 
-            var zone = await _context.Zone
-                .FirstOrDefaultAsync(m => m.ZoneId == id);
+            var zone = _zonesRepository.GetById(id);
             if (zone == null)
             {
                 return NotFound();
@@ -129,20 +130,20 @@ namespace DeviceManagement_WebApp.Controllers
             return View(zone);
         }
 
-        // POST: Zones/Delete/5
+        // POST: Delete() method that gets a Zone by its id, then delete it by calling the Remove() method
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
-            var zone = await _context.Zone.FindAsync(id);
-            _context.Zone.Remove(zone);
-            await _context.SaveChangesAsync();
+            var zone = _zonesRepository.GetById(id);
+            _zonesRepository.Remove(zone);
             return RedirectToAction(nameof(Index));
         }
 
         private bool ZoneExists(Guid id)
         {
-            return _context.Zone.Any(e => e.ZoneId == id);
+            // reference of method created in repository class
+            return _zonesRepository.ZoneExists(id);
         }
     }
 }
