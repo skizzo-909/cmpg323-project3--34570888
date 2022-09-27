b10db09e5164b25cf7fa0b0a174abac403f6b1df
skizzo-909 <nmlambo07@gmail.com>
2022-09-27 16:17:51 +0200

    Added Zones Repository Interface and Class; Fixed Dependency Injection Issue

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZonesRepository.cs
@@ -0,0 +1,18 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZonesRepository : GenericRepository<Zone>, IZonesRepository
+    {
+        public ZonesRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(service => service.DateCreated).FirstOrDefault();
+        }
+    }
+}
